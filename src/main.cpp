7fb4773bb50d22c94312d03ccb08cd38a95f510e
Riyadh Alali <eng.riyadalali93@gmail.com>
2024-06-30 14:59:52 +0300

    added esp state connection

--- src/main.cpp
+++ src/main.cpp
@@ -2,90 +2,54 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-
-const char* ap_ssid = "ESP8266_AP";
-const char* ap_password = "12345678";
-
-// Set web server port number to 80
 ESP8266WebServer server(80);
 
-#define LED 2 
-//-----------------------------------------FUNCTIONS DEFINE-----------------------------------
-void configWiFi();
-void clientWiFi();
-void config();
+void handleRoot() {
+  String page = "<form action='/connect' method='POST'>"
+                "SSID: <input type='text' name='ssid'><br>"
+                "Password: <input type='text' name='password'><br>"
+                "<input type='submit' value='Connect'></form>";
+  server.send(200, "text/html", page);
+}
 
-//-----------------------------------------FUNCTIONS------------------------------------------
-void configWiFi()
-{
- // Start the access point
-  WiFi.softAP(ap_ssid, ap_password);
-  Serial.println("Access Point started");
+void handleConnect() {
+  String ssid = server.arg("ssid");
+  String password = server.arg("password");
+
+  WiFi.begin(ssid.c_str(), password.c_str());
+
+  server.send(200, "text/html", "Connecting to WiFi...");
+}
+
+void handleStatus() {
+  String status;
+  if (WiFi.status() == WL_CONNECTED) {
+    status = "Connected";
+  } else {
+    status = "Not connected";
+  }
+  server.send(200, "text/plain", status);
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  // Set up as a soft access point
+  WiFi.softAP("ESP8266_AP", "12345678");
+
+  // Enable station mode to connect to router
+  WiFi.mode(WIFI_AP_STA);
+
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/connect", HTTP_POST, handleConnect);
+  server.on("/status", HTTP_GET, handleStatus);
+
+  server.begin();
+  Serial.println("Server started");
   Serial.print("IP Address: ");
   Serial.println(WiFi.softAPIP());
-
-  // Define the handler for the root URL
-  server.on("/", HTTP_GET, []() {
-    server.send(200, "text/html", "<form action='/connect' method='POST'>SSID: <input type='text' name='ssid'><br>Password: <input type='text' name='password'><br><input type='submit' value='Connect'></form>");
-  });
-
-  // Define the handler for the connect URL
-  server.on("/connect", HTTP_POST, []() {
-    String ssid = server.arg("ssid");
-    String password = server.arg("password");
-
-    String response = "Received SSID: " + ssid + "<br>Received Password: " + password;
-    server.send(200, "text/html", response);
-
-    // Connect to the target Wi-Fi network
-    WiFi.begin(ssid.c_str(), password.c_str());
-
-    int attempts = 0;
-    while (WiFi.status() != WL_CONNECTED && attempts < 20) {
-      delay(500);
-      Serial.print(".");
-      attempts++;
-    }
-
-    if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("Connected to Wi-Fi");
-      Serial.print("IP Address: ");
-      Serial.println(WiFi.localIP());
-    } else {
-      Serial.println("Failed to connect to Wi-Fi");
-    }
-  });
-
-  // Define the handler to check connection status
-  server.on("/status", HTTP_GET, []() {
-    String status;
-    if (WiFi.status() == WL_CONNECTED) {
-      status = "Connected to Wi-Fi. IP: " + WiFi.localIP().toString();
-    } else {
-      status = "Failed to connect to Wi-Fi.";
-    }
-    server.send(200, "text/plain", status);
-  });
-
-  // Start the server
-  server.begin();
-}
-
-//--------------------------------------------CONFIGURATION------------------------------
-void config()
-{
-  Serial.begin(9600); 
-  pinMode(LED,OUTPUT); 
-}
-//---------------------------------------------MAIN-------------------------------------
-void setup() {
-
-config();
-configWiFi(); // connecting to wifi 
-
 }
 
 void loop() {
-server.handleClient();
-
+  server.handleClient();
 }
